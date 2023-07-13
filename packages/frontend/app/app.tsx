74d47095aea8e6415e3d3293c4f034cadea5e3ac
Mike Valeriano <mkvlrn@gmail.com>
2023-07-13 20:49:43 -0300

    feat(frontend): adapt mantine template to app

--- packages/frontend/app/app.tsx
+++ packages/frontend/app/app.tsx
@@ -1,24 +1,65 @@
-import { useEffect } from 'react';
+import { Anchor, Button, Container, Group, List, rem, Text, ThemeIcon, Title } from '@mantine/core';
+import { IconCheck, IconExternalLink, IconVideo } from '@tabler/icons-react';
+import { useStyles } from '#/frontend/app/app.style.js';
 
 export function App() {
-  useEffect(() => {
-    async function getHealthy() {
-      const response = await fetch('/api/healthcheck');
-      if (!response.ok) {
-        throw new Error('Failed to get health');
-      }
-
-      const { healthy } = await response.json();
-
-      console.log('healthy', healthy);
-    }
-
-    getHealthy();
-  }, []);
+  const { classes } = useStyles();
 
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <Container>
+      <div className={classes.inner}>
+        <div className={classes.content}>
+          <Title className={classes.title}>
+            A <span className={classes.highlight}>única</span> plataforma que você precisa
+          </Title>
+          <Text color='dimmed' mt='md'>
+            Cadastre-se para ser um dos primeiros a ter acesso ao produto digital definitivo. Nada
+            vai ser como antes. A mudança de paradigmas, mentes e corações está chegando.
+          </Text>
+
+          <List
+            mt={30}
+            spacing='sm'
+            size='sm'
+            icon={
+              <ThemeIcon size={20} radius='xl'>
+                <IconCheck size={rem(12)} stroke={1.5} />
+              </ThemeIcon>
+            }
+          >
+            <List.Item>
+              <b>Extra sensorial</b> - transcenda o material e o mundano. Atinja o inalcalçável.
+              Seja o inesperado.
+            </List.Item>
+            <List.Item>
+              <b>Sem radicais livres</b> - compreensivo, porém simples de absorver. Nunca antes uma
+              tecnologia tão revolucionária foi tão acessível.
+            </List.Item>
+            <List.Item>
+              <b>Amigo dos animais</b> - e quando a convergência se tornar inevitável, você estará
+              em boa companhia.
+            </List.Item>
+          </List>
+
+          <Group mt={30}>
+            <Button radius='xl' size='md' className={classes.control} rightIcon={<IconVideo />}>
+              Testemunhos
+            </Button>
+            <Anchor href='https://github.com/mkvlrn' target='_blank'>
+              <Button
+                variant='default'
+                radius='xl'
+                size='md'
+                className={classes.control}
+                rightIcon={<IconExternalLink />}
+              >
+                Saiba mais
+              </Button>
+            </Anchor>
+          </Group>
+        </div>
+        <div>form here</div>
+      </div>
+    </Container>
   );
 }
--- packages/frontend/index.tsx
+++ packages/frontend/index.tsx
@@ -1,3 +1,4 @@
+import { MantineProvider } from '@mantine/core';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from '#/frontend/app/app.jsx';
@@ -5,6 +6,8 @@ import { App } from '#/frontend/app/app.jsx';
 const root = createRoot(document.querySelector('#root') as HTMLElement);
 root.render(
   <StrictMode>
-    <App />
+    <MantineProvider theme={{ colorScheme: 'dark' }} withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
   </StrictMode>,
 );
