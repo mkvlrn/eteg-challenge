e13fcc09b646a63302b229bfe50ebd5606f930e8
Mike Valeriano <mkvlrn@gmail.com>
2023-07-13 14:04:50 -0300

    feat: add catch all error handler for router

--- packages/backend/api/router.ts
+++ packages/backend/api/router.ts
@@ -1,6 +1,13 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { ResourceNotFound } from '#/backend/api/custom-errors.js';
 import { healtcheckHandler } from '#/backend/modules/healthcheck/healthcheck.controller.js';
 import { healthcheckSchema } from '#/backend/modules/healthcheck/healthcheck.schema.js';
+
+export async function catchAllRoute(request: FastifyRequest, _reply: FastifyReply) {
+  const { url } = request;
+
+  throw new ResourceNotFound(`path ${url} not found`);
+}
 
 export async function router(fastify: FastifyInstance) {
   // healthcheck
@@ -9,5 +16,12 @@ export async function router(fastify: FastifyInstance) {
     url: '/healthcheck',
     schema: healthcheckSchema,
     handler: healtcheckHandler,
+  });
+
+  // catch-all route
+  fastify.route({
+    method: 'GET',
+    url: '*',
+    handler: catchAllRoute,
   });
 }
