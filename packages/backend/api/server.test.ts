c95ea76617ce6ce0a88dc275151e1d96fcef7e54
Mike Valeriano <mkvlrn@gmail.com>
2023-07-13 13:34:48 -0300

    test(backend): cover all server creation scenarios

--- packages/backend/api/server.test.ts
+++ packages/backend/api/server.test.ts
@@ -1,7 +1,16 @@
-import { expect, test } from 'vitest';
+import { expect, test, vi } from 'vitest';
 import { getServer } from '#/backend/api/server.js';
 
-test('server is created', async () => {
+test('server is created when NODE_ENV is development', async () => {
+  vi.stubEnv('NODE_ENV', 'development');
+  const server = await getServer();
+
+  expect(server).toBeDefined();
+});
+
+test('server is created when NODE_ENV is production', async () => {
+  vi.stubEnv('NODE_ENV', 'production');
+
   const server = await getServer();
 
   expect(server).toBeDefined();
