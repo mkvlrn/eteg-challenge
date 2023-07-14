327a33c799f0a6fb40fb3c7d806ec505a44fb4c1
Mike Valeriano <mkvlrn@gmail.com>
2023-07-14 05:35:06 -0300

    chore: adjust vite build to use relative path for asset imports

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   // @ts-expect-error, because of esm
   plugins: [react()],
+  base: '',
   resolve: {
     alias: {
       '#/backend': resolve('.', './packages/backend'),
