2ae23768d0b8ced909877f28249301dd73d22eee
Mike Valeriano <mkvlrn@gmail.com>
2023-07-14 07:19:24 -0300

    refactor(frontend): move state logic out of form component

--- packages/frontend/components/form-state.ts
+++ packages/frontend/components/form-state.ts
@@ -1,4 +1,6 @@
-import { useForm } from '@mantine/form';
+import { useForm, UseFormReturnType } from '@mantine/form';
+import { notifications } from '@mantine/notifications';
+import { useState } from 'react';
 
 type Color = 'VERMELHO' | 'LARANJA' | 'AMARELO' | 'VERDE' | 'AZUL' | 'ANIL' | 'VIOLETA';
 
@@ -20,7 +22,25 @@ export interface FormValues {
   obs: string;
 }
 
+function setErrors(
+  form: UseFormReturnType<FormValues, (values: FormValues) => FormValues>,
+  messages: string[],
+) {
+  for (const [key] of Object.entries(form.values)) {
+    const errorMessage = messages.find((message) => message.startsWith(key));
+    if (errorMessage) form.setFieldError(key, errorMessage);
+  }
+
+  if (messages.length > 0)
+    notifications.show({
+      title: 'Erro de validação no formulário (VALIDATION_ERROR)',
+      message: 'Corrija os campos indicados e tente novamente.',
+      color: 'red',
+    });
+}
+
 export function useJoeForm() {
+  const [loading, setLoading] = useState(false);
   const form = useForm<FormValues>({
     initialValues: {
       nome: '',
@@ -36,5 +56,35 @@ export function useJoeForm() {
     }),
   });
 
-  return { form, colors };
+  const submitForm = async (values: FormValues) => {
+    setLoading(true);
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    const data: { statusCode: number; code: string; message: string | string[] } =
+      await response.json();
+    if (response.ok) {
+      notifications.show({
+        title: 'Cadastro realizado com sucesso!',
+        message: 'Em breve entraremos em contato com mais informações.',
+        color: 'green',
+      });
+      form.reset();
+    } else if (data.code === 'VALIDATION_ERROR') {
+      setErrors(form, data.message as string[]);
+    } else {
+      notifications.show({
+        title: `Erro ao enviar formulário (${data.code})`,
+        message: data.message as string,
+        color: 'red',
+      });
+    }
+
+    setLoading(false);
+  };
+
+  return { form, colors, loading, submitForm };
 }
--- packages/frontend/components/form.tsx
+++ packages/frontend/components/form.tsx
@@ -9,68 +9,20 @@ import {
   Textarea,
   TextInput,
 } from '@mantine/core';
-import { notifications } from '@mantine/notifications';
-import { FormValues, useJoeForm } from '#/frontend/components/form-state.js';
+import { useJoeForm } from '#/frontend/components/form-state.js';
 
 export function UserForm() {
-  const { form, colors } = useJoeForm();
-
-  function setErrors(messages: string[]) {
-    const nomeError = messages.find((message) => message.startsWith('nome'));
-    const emailError = messages.find((message) => message.startsWith('email'));
-    const cpfError = messages.find((message) => message.startsWith('cpf'));
-    const corFavoritaError = messages.find((message) => message.startsWith('corFavorita'));
-    const obsError = messages.find((message) => message.startsWith('obs'));
-
-    if (nomeError) form.setFieldError('nome', nomeError);
-    if (emailError) form.setFieldError('email', emailError);
-    if (cpfError) form.setFieldError('cpf', cpfError);
-    if (corFavoritaError) form.setFieldError('corFavorita', corFavoritaError);
-    if (obsError) form.setFieldError('obs', obsError);
-
-    if (messages.length > 0)
-      notifications.show({
-        title: 'Erro de validação no formulário (VALIDATION_ERROR)',
-        message: 'Corrija os campos indicados e tente novamente.',
-        color: 'red',
-      });
-  }
-
-  async function handleSubmit(values: FormValues) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify(values),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    const data: { statusCode: number; code: string; message: string | string[] } =
-      await response.json();
-    if (response.ok) {
-      notifications.show({
-        title: 'Cadastro realizado com sucesso!',
-        message: 'Em breve entraremos em contato com mais informações.',
-        color: 'green',
-      });
-      form.reset();
-    } else if (data.code === 'VALIDATION_ERROR') {
-      setErrors(data.message as string[]);
-    } else {
-      notifications.show({
-        title: `Erro ao enviar formulário (${data.code}))`,
-        message: data.message as string,
-        color: 'red',
-      });
-    }
-  }
+  const { form, colors, loading, submitForm } = useJoeForm();
 
   return (
     <Container w='60rem'>
       <Paper withBorder shadow='md' p={25} radius='md'>
-        <form onSubmit={form.onSubmit(handleSubmit)}>
+        <form onSubmit={form.onSubmit(submitForm)}>
           <Stack spacing='xl'>
             <TextInput
               label='Nome completo'
               placeholder='Seu nome'
+              disabled={loading}
               {...form.getInputProps('nome')}
               withAsterisk
             />
@@ -78,6 +30,7 @@ export function UserForm() {
             <TextInput
               label='Email'
               placeholder='seu@email.com.br'
+              disabled={loading}
               {...form.getInputProps('email')}
               withAsterisk
             />
@@ -86,6 +39,7 @@ export function UserForm() {
               label='CPF (somente números)'
               placeholder='000.000.000-00'
               maxLength={11}
+              disabled={loading}
               {...form.getInputProps('cpf')}
               withAsterisk
             />
@@ -98,22 +52,29 @@ export function UserForm() {
               swatches={Object.keys(colors)}
               format='hex'
               closeOnColorSwatchClick
+              disabled={loading}
               {...form.getInputProps('corFavorita')}
               withAsterisk
             />
             <Textarea
               label='Observações'
               placeholder='Lorem ipsum'
+              disabled={loading}
               {...form.getInputProps('obs')}
             />
           </Stack>
           <Group mt='xl' grow>
-            <Button type='submit'>
+            <Button type='submit' loading={loading}>
               <Text weight='bold' size='lg'>
                 Cadastrar
               </Text>
             </Button>
-            <Button type='reset' color='gray' onClick={form.reset} disabled={!form.isDirty()}>
+            <Button
+              type='reset'
+              color='gray'
+              onClick={form.reset}
+              disabled={!form.isDirty() || loading}
+            >
               Limpar
             </Button>
           </Group>
